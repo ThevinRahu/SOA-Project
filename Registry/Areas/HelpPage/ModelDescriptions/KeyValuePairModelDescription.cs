2cafb256a547d8bd1e9f37ca54cab3034a5a6fad
ThevinRahu <thevinvr@gmail.com>
2022-09-05 11:38:39 +0530

    Projects created

---
+++ Registry/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Registry.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
