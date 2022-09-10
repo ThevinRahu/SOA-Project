4333e8d421d951459d8b09c3693361c282829bc9
ThevinRahu <thevinvr@gmail.com>
2022-09-11 00:49:07 +0530

    service provider done, registery publish and search done

---
+++ Registry/Controllers/SearchController.cs
@@ -0,0 +1,55 @@
+﻿using Authenticator;
+using Newtonsoft.Json;
+using Registry.Models;
+using ServiceProvider.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Registry.Controllers
+{
+    public class SearchController : ApiController
+    {
+        InterfaceChannel iChannel = new InterfaceChannel();
+        AuthenticatorInterface iserverChannel;
+        public IHttpActionResult Post([FromBody] string description, [FromUri()] int token)
+        {
+            string servicelocation = Directory.GetCurrentDirectory() + @"..\..\Services\services.txt";
+            StreamReader reader = new StreamReader(servicelocation);
+            iserverChannel = iChannel.generateChannel();
+            string validateResult = iserverChannel.Validate(token);
+
+            if (validateResult == "Validated")
+            {
+                /*while ((reader.ReadLine()) != null)
+                {
+                    
+
+                } */
+                List<string> lines = new List<string>();
+                lines = File.ReadAllLines(servicelocation).ToList();
+                List<string> data = new List<string>();
+                for (int i = 0; i < lines.Count; i = ++i * 7)
+                {
+                    Service services = JsonConvert.DeserializeObject<Service>(lines[i+1]);
+                    if (services.description.Contains(description))
+                    {
+                        data.Add(JsonConvert.SerializeObject(services));
+                    }
+                }
+                return Ok(data);
+            }
+            else
+            {
+                AuthFail af = new AuthFail("Denied", "Authentication Error");
+                return Ok(af);
+            }
+
+        
+        }
+    }
+}
---
+++ ServiceProvider/Models/InterfaceChannel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using Authenticator;
+
+namespace ServiceProvider.Models
+{
+    public class InterfaceChannel
+    {
+        private AuthenticatorInterface interfaceChannel;
+        public AuthenticatorInterface generateChannel()
+        {
+            ChannelFactory<AuthenticatorInterface> channelFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+            string URL = "net.tcp://localhost:8100/AuthenticationService";
+            channelFactory = new ChannelFactory<AuthenticatorInterface>(tcp, URL);
+            interfaceChannel = channelFactory.CreateChannel();
+            return interfaceChannel;
+        }
+
+    }
+}
